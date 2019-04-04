ded58cf6718a77dce0355a4a0817678fc680d954
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2019-04-04 12:21:19 +0200

    Update orange.R

--- orange.R
+++ orange.R
@@ -1,8 +1,8 @@
 ## Orange Juice Pricing ##
 
 # Importing data #
-url1 = 'https://raw.githubusercontent.com/mcanela-iese/'
-url2 = 'Quants_Book/master/orange.csv'
+url1 = 'https://raw.githubusercontent.com/quants-book/'
+url2 = 'CSV_Files/master/orange.csv'
 url = paste0(url1, url2)
 df = read.csv(url)
 str(df)
