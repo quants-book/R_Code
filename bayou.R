5d43133d535e39d79a2dec3e6f338b9fc6d8dfbe
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2019-04-04 12:14:58 +0200

    Update bayou.R

--- bayou.R
+++ bayou.R
@@ -1,8 +1,8 @@
 ## Bayou Beer Sales ##
 
 # Importing data #
-url1 = 'https://raw.githubusercontent.com/mcanela-iese/'
-url2 = 'Quants_Book/master/bayou.csv'
+url1 = 'https://raw.githubusercontent.com/quants-book/'
+url2 = 'CSV_Files/master/bayou.csv'
 url = paste0(url1, url2)
 df = read.csv(url, stringsAsFactors=FALSE)
 str(df)
