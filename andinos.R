efce2fa78f4751510337e548d79b70176d4d69de
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2019-04-04 12:13:59 +0200

    Update andinos.R

--- andinos.R
+++ andinos.R
@@ -1,8 +1,8 @@
 ## Supermercados Andinos ##
 
 # Importing data #
-url1 = 'https://raw.githubusercontent.com/mcanela-iese/'
-url2 = 'Quants_Book/master/andinos.csv'
+url1 = 'https://raw.githubusercontent.com/quants-book/'
+url2 = 'CSV_Files/master/andinos.csv'
 url = paste0(url1, url2)
 df = read.csv(url, stringsAsFactors=FALSE)
 str(df)
