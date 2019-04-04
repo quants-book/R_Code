4a5e81a42b59bdea8dd1d35db4fe49e63f5b7f56
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2019-04-04 12:20:08 +0200

    Update gender.R

--- gender.R
+++ gender.R
@@ -1,8 +1,8 @@
 ## Gender Discrimination ##
 
 # Importing data #
-url1 = 'https://raw.githubusercontent.com/mcanela-iese/'
-url2 = 'Quants_Book/master/gender.csv'
+url1 = 'https://raw.githubusercontent.com/quants-book/'
+url2 = 'CSV_Files/master/gender.csv'
 url = paste0(url1, url2)
 df = read.csv(url)
 str(df)
