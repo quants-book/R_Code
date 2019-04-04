b32a591b1057fb0ccabb60d74512ffc281f904ea
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2019-04-04 12:18:45 +0200

    Update diesel.R

--- diesel.R
+++ diesel.R
@@ -1,8 +1,8 @@
 ## Diesel Consumption ##
 
 # Importing data #
-url1 = 'https://raw.githubusercontent.com/mcanela-iese/'
-url2 = 'Quants_Book/master/diesel.csv'
+url1 = 'https://raw.githubusercontent.com/quants-book/'
+url2 = 'CSV_Files/master/diesel.csv'
 url = paste0(url1, url2)
 df = read.csv(url)
 str(df)
